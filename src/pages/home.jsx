64c46964d0a03b661d4890a2036c95d5dba05412
Michael Owuor <michael.owuor@gmail.com>
2022-10-19 21:08:48 +0300

    feat: trending coins component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import CoinSearch from '../components/coin-search';
+import Trending from '../components/trending-coins';
 
 const Home = ({ coins }) => {
 	return (
 		<div>
 			<CoinSearch coins={coins} />
+			<Trending />
 		</div>
 	);
 };
