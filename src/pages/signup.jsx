bacd36c9b08bf7debd487c8af75aabec3244b6df
Michael Owuor <michael.owuor@gmail.com>
2022-10-20 11:08:43 +0300

    feat: signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,4 +1,62 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AiFillLock, AiOutlineMail } from 'react-icons/ai';
+
 const Signup = () => {
-	return <div>Sign Up Page</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const navigate = useNavigate();
+
+	const handleSubmit = () => {};
+
+	return (
+		<div>
+			<div className="mx-auto min-h-[600px] max-w-[400px] px-4 py-20">
+				<h1 className="text-2xl font-bold">Sign Up</h1>
+
+				{error ? <p className="my-2 bg-red-300 p-3">{error}</p> : null}
+
+				<form onSubmit={handleSubmit}>
+					<div className="my-4">
+						<label>Email</label>
+						<div className="relative my-2 w-full rounded-2xl shadow-xl">
+							<input
+								onChange={(e) => setEmail(e.target.value)}
+								className="w-full rounded-2xl border border-input bg-primary p-2"
+								type="email"
+							/>
+							<AiOutlineMail className="absolute right-2 top-3 text-gray-500" />
+						</div>
+					</div>
+
+					<div className="my-4">
+						<label>Password</label>
+						<div className="relative my-2 w-full rounded-2xl shadow-xl">
+							<input
+								onChange={(e) => setPassword(e.target.value)}
+								className="w-full rounded-2xl border border-input bg-primary p-2"
+								type="password"
+							/>
+							<AiFillLock className="absolute right-2 top-3 text-gray-500" />
+						</div>
+					</div>
+
+					<button className="my-2 w-full rounded-2xl bg-button p-3 text-btnText shadow-xl">
+						Sign up
+					</button>
+				</form>
+
+				<p className="my-4">
+					Already have an account?{' '}
+					<Link to="/signin" className="text-accent">
+						Sign in
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
 };
+
 export default Signup;
